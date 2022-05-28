36e23698ace46d6d39bed4568ff1b7c08ef60625
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-28 22:21:09 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,28 @@
+// Pattern Program 4
+#include <stdio.h>
+
+int main()
+{
+    int i,j;
+    for(i=5; i>=1;i--)
+    {
+        for(j=1;j<=5;j++)
+        {
+            printf("%d",i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
+/*
+
+55555
+44444
+33333
+22222
+11111
+
+
+*/
