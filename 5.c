4a95d34e88215b558a0d8137eb704eddb7e1e8b4
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-28 22:21:59 +0530

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,28 @@
+// Pattern Program 5
+#include <stdio.h>
+
+int main()
+{
+    int i,j;
+    for(i=5; i>=1;i--)
+    {
+        for(j=5;j>=1;j--)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
+/*
+
+54321
+54321
+54321
+54321
+54321
+
+
+*/
